f2c083d01918979ebbe9ec7b832ee2853edfb4c8
Taleemj <taleemmankuer@gmail.com>
2025-12-18 10:57:36 +0300

    remove console log

--- api/dashboard/useDashboard.tsx
+++ api/dashboard/useDashboard.tsx
@@ -4,7 +4,6 @@ import { Counters } from "../types/dashboard";
 const useDashboard = () => {
   const fetchDashboard = async (): Promise<Counters> => {
     const { data } = await client.get("/api/delivery/dashboard");
-    console.log("dashobard", data);
     return data;
   };
   return { fetchDashboard };
